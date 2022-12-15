3fa99c87d920f770053487c36c2421b39ec96337
christianriccio <riccio.christian.21@gmail.com>
2022-12-15 11:11:30 +0100

    second commit

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv('./.flaskenv')
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1> Hello world </h1>'
 
 if __name__ == '__main':
     app.run()
