f427e3110195cd74fb20a2bc9ac71e90efcb8727
christianriccio <riccio.christian.21@gmail.com>
2022-12-27 17:31:38 +0100

    created a module structure

---
+++ backend/app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from dotenv import load_dotenv
+
+from flask_sqlalchemy import SQLAlchemy
+from app.config import Config
+from flask_migrate import Migrate
+
+load_dotenv('./.flaskenv')
+
+app = Flask(__name__, template_folder='templates')
+#app.config['SECRET_KEY'] = 'cbf47b2eab3d3ecb0ba15b46a7e91842'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes
