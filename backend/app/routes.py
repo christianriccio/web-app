05999e4cab193800f3a3361794c905e217bb9bbc
christianriccio <riccio.christian.21@gmail.com>
2022-12-28 17:14:17 +0100

    creted logout route

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from app import app, db, bcrypt
 from app.models import User, Post
 from app.forms import RegistrationForm, LoginForm
-from flask_login import login_user
+from flask_login import login_user, current_user, logout_user
 
 posts = [
     {
@@ -30,6 +30,8 @@ def about():
 
 @app.route('/register', methods = ['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -42,12 +44,19 @@ def register():
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email = form.email.data).first()
-        if user and bcrypt.check_password_hash(user.paswword, form.password.data):
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember= form.remember.data)
             return redirect(url_for('home'))
         else:
             flash('Login failed, please check email and password', 'danger')
     return render_template('login.html', title = 'Login', form = form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
