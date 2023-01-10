03d9274549dfc86cd277d6efbfbb673a7353ce4d
christianriccio <riccio.christian.21@gmail.com>
2023-01-10 16:15:17 +0100

    modular application

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -18,7 +18,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 #app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 #app.config['MAIL_PORT'] = 465 #587
@@ -35,4 +35,11 @@ app.config['MAIL_USE_SSL'] = False
 mail = Mail(app)
 migrate = Migrate(app, db)
 
-from app import routes
+
+from app.users.routes import users
+from app.main.routes import main
+from app.posts.routes import posts
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
+
