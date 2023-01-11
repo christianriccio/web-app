b8e0e9be7efc397d1ebd63b0b26c99d771d94b00
christianriccio <riccio.christian.21@gmail.com>
2023-01-11 09:48:18 +0100

    change project structure

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -9,25 +9,34 @@ from flask_migrate import Migrate
 
 load_dotenv('./.flaskenv')
 
-app = Flask(__name__, template_folder='templates')
+
 #app.config['SECRET_KEY'] = 'cbf47b2eab3d3ecb0ba15b46a7e91842'
 #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
+mail = Mail()
+#migrate = Migrate(, db)
 
-mail = Mail(app)
-migrate = Migrate(app, db)
 
+def create_app(config_class=Config):
+    app = Flask(__name__, template_folder='templates')
+    app.config.from_object(Config)
 
-from app.users.routes import users
-from app.main.routes import main
-from app.posts.routes import posts
-app.register_blueprint(users)
-app.register_blueprint(posts)
-app.register_blueprint(main)
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app) 
+    from app.users.routes import users
+    from app.main.routes import main
+    from app.posts.routes import posts
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
